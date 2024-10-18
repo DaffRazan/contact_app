09af0238a44be643fe8547b6385e0850dca7df44
Muhammad Daffa Razan <m.daffa@idgi.co.id>
2024-10-19 06:54:29 +0700

    feat: add color theme

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppThemeColor {
-  Color bluePrimary = const Color(0xFF0077B6);
-  Color white = const Color(0xFFFFFFFF);
-  Color black = const Color(0xFF000000);
-  Color darkGray = const Color(0xFFCCCCCC);
-  Color lightGray = const Color(0xFFEBEBEB);
-  Color red = const Color(0xFFED1C2E);
+  static Color bluePrimary = const Color(0xFF0077B6);
+  static Color white = const Color(0xFFFFFFFF);
+  static Color black = const Color(0xFF000000);
+  static Color darkGray = const Color(0xFFCCCCCC);
+  static Color lightGray = const Color(0xFFEBEBEB);
+  static Color red = const Color(0xFFED1C2E);
 }
