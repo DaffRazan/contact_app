51cb5cc1b19470cc7175220b05ec1cfd331f4723
Muhammad Daffa Razan <m.daffa@idgi.co.id>
2024-10-19 06:55:07 +0700

    feat: implement poppins font and material get

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:contact_app/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,12 +10,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Contact App',
       theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
